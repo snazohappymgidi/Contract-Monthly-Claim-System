5f6a54cbd62610401d63bfb072d19e94ff4839e8
snazohappymgidi <st10220472@imconnect.edu.za>
2024-09-09 23:47:11 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Contract_Monthly_Claim_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
